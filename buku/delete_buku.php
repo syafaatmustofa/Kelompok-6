605e03fe478e40db5922d8e5fef14d2ed42b9b40
syafaat <112368701+syafaatmustofa@users.noreply.github.com>
2022-10-02 05:24:23 +0700

    Update delete_buku.php

---
+++ buku/delete_buku.php
@@ -0,0 +1,21 @@
+<?php
+include '../config.php';
+
+$id = $_GET['id_buku'];
+$qdata = mysqli_query($db, "SELECT cover FROM buku WHERE id_buku = '$id'");
+$data = mysqli_fetch_array($qdata);
+$delete = mysqli_query($db, "DELETE FROM buku WHERE id_buku='$id'");
+
+unlink("./../asset/bootstrap/img/" . $data['cover']);
+
+if ($delete) { ?>
+    <script>
+        alert("Berhasil menghapus!");
+        document.location = "home_buku.php";
+    </script>
+<?php
+} else {
+    echo "gagal menghapus";
+}
+
+?>
