f310637f5fb497e9c52b9f6081e31ab0efcd5edb
syafaat <112368701+syafaatmustofa@users.noreply.github.com>
2022-10-02 05:40:49 +0700

    Update editproses_buku.php

--- buku/editproses_buku.php
+++ buku/editproses_buku.php
@@ -11,7 +11,7 @@ $sinopsis = $_POST['sinopsis'];
 $stok = $_POST['stok'];
 $cover = $_FILES['cover']['name'];
 $tmp_name = $_FILES['cover']['tmp_name'];
-move_uploaded_file($tmp_name, "./../assets/bootstrap/img/".$cover);
+move_uploaded_file($tmp_name, "./../assets/img/".$cover);
 
 $query = mysqli_query($db, "UPDATE buku SET penulis='$penulis', tahun='$tahun', judul='$judul', kota='$kota', penerbit='$penerbit', cover='$cover', sinopsis='$sinopsis', stok='$stok' WHERE id_buku='$id_buku'");
 
