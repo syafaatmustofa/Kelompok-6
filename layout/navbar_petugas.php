2e6439b7c6fe2e8f710fff2e2229f5b4b8672b27
Nur Hidayatul Fatihah <nurhidayatulfatihah19@gmail.com>
2022-10-03 11:42:37 +0700

    rename layout

--- layout/navbar_petugas.php
+++ layout/navbar_petugas.php
@@ -19,7 +19,7 @@
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="../admin/logout_admin.php">Logout</a></li>
+                <li><a class="dropdown-item" href="../admin/logout_petugas.php">Logout</a></li>
             </ul>
         </li>
     </ul>
--- peminjaman/tambah_peminjaman.php
+++ peminjaman/tambah_peminjaman.php
@@ -15,11 +15,11 @@ include "../layout/header.php";
 
 <body class="sb-nav-fixed">
     <?php 
-    include "../layout/navbar_admin.php";
+    include "../layout/navbar_petugas.php";
     ?>
     <div id="layoutSidenav">
         <?php
-            include "../layout/sidebar_admin.php";
+            include "../layout/sidebar_petugas.php";
             ?>
     </div>
     <div id="layoutSidenav_content" class="w-75" style="position: relative; left: 20%; margin-top: 100px;">
